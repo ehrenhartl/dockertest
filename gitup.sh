9e7bc3af3f5b7e30415efab7ab652d8db64f90ef
ehrenhartl <leonehrenhart@googlemail.com>
2016-06-14 12:27:26 +0000

    final

--- gitup.sh
+++ gitup.sh
@@ -3,12 +3,14 @@ read -p "Do you have already set the remote branch?" yn
 case $yn in
 [Yy]* ) 
 git add .
-git commit -m "e"
+
+echo "Please enter the commitmessage:"
+read userinput
+
+git commit -m "$userinput"
 git push -u origin master; break;;
 [Nn]* ) exit;;
 * ) echo "Please answer yes or no.";;
 esac
 done
 
-
-echo "upload completed"
