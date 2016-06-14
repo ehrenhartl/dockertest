acb67c2a69f6ae828c02864eac777059007aaa58
ehrenhartl <leonehrenhart@googlemail.com>
2016-06-14 12:20:02 +0000

    e

---
+++ gitinit.sh
@@ -0,0 +1,5 @@
+git init
+git config --global user.name "ehrenhartl"
+git config --global user.email "leonehrenhart@googlemail.com"
+git remote add origin https://github.com/ehrenhartl/dockertest.git
+
---
+++ gitup.sh
@@ -0,0 +1,14 @@
+while true; do
+read -p "Do you have already set the remote branch?" yn
+case $yn in
+[Yy]* ) 
+git add .
+git commit -m "e"
+git push -u origin master; break;;
+[Nn]* ) exit;;
+* ) echo "Please answer yes or no.";;
+esac
+done
+
+
+echo "upload completed"
